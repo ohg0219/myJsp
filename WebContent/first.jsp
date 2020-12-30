b0cb69b9b487f31c1d1b0771e1da768a4aa28a5d
Hyung-gyun Oh <69466163+ohg0219@users.noreply.github.com>
2020-12-30 10:06:03 +0900

    test code

---
+++ WebContent/first.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>My First Jsp</title>
+</head>
+<body>
+	<h2>My First Jsp Test File</h2>
+	<hr color="red">
+	<br>
+	<p align="center">
+		지금은
+		<%=new java.util.Date()%>입니다.
+	</p>
+</body>
+</html>
---
+++ src/com/FirstServlet.java
@@ -0,0 +1,39 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		try{
+			out.println("<html><head><title>My First Servlet</title></head><body>");
+			out.println("<h2>My First Servlet File</h2><hr color='red'><br>");
+			out.println("<p align='center'>지금은 ");
+			out.println(new java.util.Date());
+			out.println("입니다. </p></body></html>");
+		}finally{
+			out.close();
+		}
+	}
+
+
+}
